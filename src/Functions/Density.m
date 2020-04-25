c3f1356e1e1603fb9eb4349baeddb4c3ac46e6f8
Louis Etienne <louis.etienne82@orange.fr>
2020-04-25 08:54:58 +0200

    Add limit at 120km for pressure and density

--- src/Functions/Density.m
+++ src/Functions/Density.m
@@ -1,6 +1,14 @@
 function rho = Density(h)
     addpath('Data');
     load('constants.mat', 'Atm');
-
-    rho = Atm.rho0 * exp(-h/Atm.hs);
+    
+    function r = tmp(hi)
+        if hi >= 120000
+            r = 0;
+        else
+            r =  Atm.rho0 * exp(-hi/Atm.hs);
+        end
+    end
+    
+    rho = arrayfun(@tmp, h);
 end
--- src/Functions/Pressure.m
+++ src/Functions/Pressure.m
@@ -1,4 +1,12 @@
 function P = Pressure(h)
-    P = 0.699 .* exp(-0.00009 .* h); % Pascal
+    function p = tmp(hi)
+        if hi >= 1200000
+            p = 0;
+        else
+            p = 0.699 .* exp(-0.00009 .* hi);
+        end
+    end
+    
+    P = arrayfun(@tmp, h);
 end
 
