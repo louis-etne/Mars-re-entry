555d92a602632087716b4620e1c89094cb649cf9
Unknown <fulminata27@gmail.com>
2020-04-26 11:02:24 +0200

    coeff normal ok

--- src/Functions/NormalForceCoef.m
+++ src/Functions/NormalForceCoef.m
@@ -1,8 +1,31 @@
-function C_N = NormalForceCoef(normal_acc, vel, h)
+function C_N = NormalForceCoef(M, alpha)
+
     addpath('Data');
-    load('constants.mat', 'Vehicle');
-    
-    rho = Density(h);
-    
-    C_N = (Vehicle.mass .* normal_acc) ./ (0.5 .* rho .* vel.^2 .* Vehicle.S);
+    load('fit_areo_norm_coef.mat', 'f_CN6', 'f_CN10', 'f_CN_04_a_23_5', 'f_CN_04_a_1_0', ...
+    'f_CN_06', 'f_CN_08', 'f_CN_1', 'f_CN_1_2', 'f_CN_2');
+
+    if M >= 8
+        C_N = f_CN10.p1 * alpha + f_CN10.p2;
+    elseif M >= 4 && M <= 8 
+        C_N = f_CN10.p1 * alpha + f_CN10.p2;
+    elseif M >= 1.5 && M <= 4
+        C_N = f_CN_2.p1 * alpha.^2 + f_CN_2.p2 * alpha + f_CN_2.p3;
+    elseif M >= 1.1 && M <= 1.5
+        C_N = f_CN_1_2.p1 * alpha.^2 + f_CN_1_2.p2 * alpha + f_CN_1_2.p3;
+    elseif M >= 0.95 && M <= 1.1 
+        C_N = f_CN_1.p1 * alpha.^2 + f_CN_1.p2 * alpha + f_CN_1.p3;
+    elseif M >= 0.7 && M <= 0.95 
+        C_N = f_CN_08.p1 * alpha.^2 + f_CN_08.p2 * alpha + f_CN_08.p3;
+    elseif M >= 0.5 && M <= 0.7 
+        C_N = f_CN_06.p1 * alpha.^2 + f_CN_06.p2 * alpha + f_CN_06.p3;
+    else 
+        if abs(alpha) >=5
+            C_N = f_CN_04_a_23_5.p1 * alpha.^2 + f_CN_04_a_23_5.p2 * alpha ...
+                + f_CN_04_a_23_5.p3;
+        elseif abs(alpha) <= 1
+            C_N = f_CN_04_a_1_0.p1 * alpha + f_CN_04_a_1_0.p2;
+        else
+            C_N = 0;
+        end
+    end
 end
